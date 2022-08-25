6f7755747deeaa03cc8b3c6a1eb88eb60509b03f
Your Name <you@example.com>
2022-08-25 11:18:44 +0800

    make it better

--- app.py
+++ app.py
@@ -246,4 +246,7 @@ def password_en():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=5050)
+    ...
+    port = int(os.environ.get('PORT', 5000))
+    ... 
+    app.run(host='0.0.0.0', debug=True, port=port)
